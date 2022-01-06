9a53ba9b1e06b45c9ab5f783952f92bf9268a255
linh <>
2022-01-06 20:58:13 +0700

    init

---
+++ MobileApp/src/redux/store.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({ reducer: rootReducer })
